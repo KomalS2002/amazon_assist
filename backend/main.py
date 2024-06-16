74d64925637f1c6623a74f2fe131b06a2694deaa
komals2002 <komalsingh10072002@gmail.com>
2024-06-16 21:43:10 +0530

    gauth working

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
 from routes.user import router
-app = FastAPI(title="AmazonAsist")
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+# app = FastAPI(title="AmazonAsist")
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",
+]
 
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+]
+app = FastAPI(middleware=middleware)
 app.include_router(router)
+
+
+
+
+
 
 @app.get("/")
 def server_started():
